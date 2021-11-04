f46c0ae6eb317f3c1995b71a41df100b84c12b6c
Dimas Silva <dimas086@gmail.com>
2021-11-04 19:28:49 -0300

    Modelo de DbFirst

---
+++ DataBaseFirst/Models/CarroDBContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace DataBaseFirst.Models
+{
+    public partial class CarroDBContext : DbContext
+    {
+        public CarroDBContext()
+        {
+        }
+
+        public CarroDBContext(DbContextOptions<CarroDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Carros> Carros { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Carros>(entity =>
+            {
+                entity.HasKey(e => e.CarroId);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
